14bcdf5857fa729d3656e9c2dd9e972a535e5681
ayub <yuanaayubs@gmail.com>
2020-03-31 20:19:45 +0700

    forceupdate on show all data

--- src/pages/AllData/allData.js
+++ src/pages/AllData/allData.js
@@ -2,79 +2,30 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import {WebView} from 'react-native-webview';
 import {connect} from 'react-redux';
-import {useIsFocused} from '@react-navigation/native';
-
-function Profile() {
-  // This hook returns `true` if the screen is focused, `false` otherwise
-  const isFocused = useIsFocused();
-
-  return <Text>{isFocused ? 'focused' : 'unfocused'}</Text>;
-}
 
 class AllData extends Component {
-  constructor(props) {
-    super(props);
-    console.log('const');
+  componentDidMount() {
+    this._unsubscribe = this.props.navigation.addListener('focus', () => {
+      this.forceUpdate();
+    });
   }
 
   componentWillUnmount() {
-    console.log('unmount');
+    this._unsubscribe();
   }
 
-  deviceId = this.props.allData[this.props.index].device_id;
   render() {
-    // console.log(JSON.stringify(this.props.allData[this.props.index], null, 2));
+    let deviceId = this.props.allData[this.props.index].device_id;
     return (
       <View style={{flex: 1}}>
-        {/* {Profile()} */}
         <WebView
           source={{
-            uri:
-              'https://iot.arduinosolo.com/angular/index.html?device_id=' +
-              this.deviceId,
+            uri: `https://iot.arduinosolo.com/angular/index.html?device_id=${deviceId}`,
           }}
-          onNavigationStateChange={this.handleWebViewNavigationStateChange}
-          // style={{marginTop: 20}}
         />
       </View>
     );
   }
-  handleWebViewNavigationStateChange = newNavState => {
-    // newNavState looks something like this:
-    // {
-    //   url?: string;
-    //   title?: string;
-    //   loading?: boolean;
-    //   canGoBack?: boolean;
-    //   canGoForward?: boolean;
-    // }
-    const {url} = newNavState;
-    if (!url) return;
-
-    // handle certain doctypes
-    if (url.includes('.pdf')) {
-      this.webview.stopLoading();
-      // open a modal with the PDF viewer
-    }
-
-    // one way to handle a successful form submit is via query strings
-    if (url.includes('?message=success')) {
-      this.webview.stopLoading();
-      // maybe close this view?
-    }
-
-    // one way to handle errors is via query string
-    if (url.includes('?errors=true')) {
-      this.webview.stopLoading();
-    }
-
-    // redirect somewhere else
-    if (url.includes('google.com')) {
-      const newURL = 'https://reactnative.dev/';
-      const redirectTo = 'window.location = "' + newURL + '"';
-      this.webview.injectJavaScript(redirectTo);
-    }
-  };
 }
 
 function mapStatetoProps(state) {
